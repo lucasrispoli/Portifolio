537f3137987b1de5e924f6419e4222d7080897c6
Lucas Rispoli Delgado <lucas.delgado@aluno.senai.br>
2025-08-29 15:40:44 -0300

    Implementação menu e adição de funcionalidades

--- index/index.js
+++ index/index.js
@@ -5,3 +5,11 @@ trilho.addEventListener('click', ()=>{
     trilho.classList.toggle('dark')
     body.classList.toggle('dark')
 })
+
+  function abrirMenu() {
+    document.getElementById("sidebar").style.width = "250px";  
+  }
+
+  function fecharMenu() {
+    document.getElementById("sidebar").style.width = "0";   
+  }

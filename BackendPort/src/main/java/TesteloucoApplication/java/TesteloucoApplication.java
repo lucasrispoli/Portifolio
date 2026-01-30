0f773cd2387ad415d45b8ffced4d780ddf3b64ab
Lucas Rispoli Delgado <162611605+lucasrispoli@users.noreply.github.com>
2026-01-30 00:31:07 -0300

    add backend logic

---
+++ BackendPort/src/main/java/TesteloucoApplication/java/TesteloucoApplication.java
@@ -0,0 +1,12 @@
+package tech.buildrun.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteloucoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TesteloucoApplication.class, args);
+    }
+}
---
+++ BackendPort/src/test/java/tech/buildrun/testelouco/TesteloucoApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.buildrun.testelouco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteloucoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
